6d1fd6b7364c907a3e0a0d8171768a6389b8e840
Karina Alvarez <alvarez.webdeveloper@gmail.com>
2020-08-21 23:45:43 -0400

    Commit and updates

---
+++ week-3/advanced-routing/alvarez-exercise-3.3.js
@@ -0,0 +1,37 @@
+const header = require('./alvarez-header.js');
+
+console.log(header.display("Karina", "Alvarez", "Exercise 3.3 - Routing"));
+console.log ('\n');
+/*
+============================================
+; Title:  app.js
+; Author: Professor Krasso
+; Date:   21 August 2020
+: Modified by: Karina Alvarez
+; Description: Demonstrates advanced routing
+;===========================================
+*/
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express();
+
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('short'));
+
+app.get('/:employeeId', function(req, res) {
+  var employeeId = parseInt(req.params.employeeId, 10);
+
+  res.render('index', {
+    employeeId: employeeId
+  });
+});
+
+http.createServer(app).listen(8000, function() {
+  console.log("Application started on port, ", 8000)
+});
--- week-3/morgan-logging/alvarez-exercise-3.2.js
+++ week-3/morgan-logging/alvarez-exercise-3.2.js
@@ -5,11 +5,38 @@ console.log ('\n');
 
 /*
 ============================================
-; Title:  Alvarez-assignment-2.4.js
+; Title: alvarez-exercise-3.2.js
 ; Author: Professor Krasso
+; Date:   20 August 2020
 ; Modified by: Karina Alvarez
-; Date:   13 August 2020
-; Description: Creates a Node.js server and returns
-;              the index.ejs page
+; Description: Demonstrates how to configure the
+;              Morgan logger
 ;===========================================
 */
+
+//start of program
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan')
+
+var app = express();
+
+// telling Express to check on views directory
+app.set('views', path.resolve(__dirname, 'views'));
+
+//telling Express to use EJS view engine
+app.set('view engine', 'ejs');
+
+app.use(logger('short'));
+
+app.get('/', function(require, response) {
+  response.render('index', {
+    message: 'Learning Morgan Logger - Exercise 3.2'
+  })
+});
+
+http.createServer(app).listen(3000, function() {
+  console.log('Application started on port,', 3000)
+})
+//end of program
